4de23d0edf1ac97ec87d3b44ad2ee08cb4973040
leanalvarez <leanalvarez.dev@gmail.com>
2024-07-02 11:46:00 -0300

    Initial Commit

---
+++ JsonApp/JsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppApp.swift
+//  JsonApp
+//
+//  Created by Lean on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
